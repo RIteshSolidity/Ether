b085e1e5065fea606c9df55b7b7139c6259c0e3e
RIteshSolidity <34639678+RIteshSolidity@users.noreply.github.com>
2020-02-18 15:16:30 +0530

    Create functiontotest.cs

---
+++ functiontotest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp20
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<string> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            return "Hello, " + name;
+        }
+    }
+}
